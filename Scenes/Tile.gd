1987b19747d6ca7a7db0860afc15cc7dd3b5c0d3
Sylvaindelaforet <74321124+Sylvaindelaforet@users.noreply.github.com>
2024-08-17 11:00:12 +0200

    fix du projet

--- Scenes/Tile.gd
+++ Scenes/Tile.gd
@@ -7,7 +7,11 @@ var i_x = -1
 var i_y = -1
 var color = null
 
+var width = 10
+var height = 10
+
 func _ready():
+	set_dimensions()
 	pass
 
 func _process(delta):
@@ -17,7 +21,8 @@ func init(_tiles_grid, _x , _y, _width, _height, _i_x, _i_y, _color, _tiles_text
 	tiles_grid = _tiles_grid
 	self.position.x = _x
 	self.position.y = _y
-	set_dimensions(_width, _height)
+	width = _width
+	height = _height
 	i_x = _i_x
 	i_y = _i_y
 	tiles_textures = _tiles_textures
@@ -27,9 +32,9 @@ func init(_tiles_grid, _x , _y, _width, _height, _i_x, _i_y, _color, _tiles_text
 func clicked():
 	tiles_grid.user_clicked_on_tile(i_x, i_y)
 
-func set_dimensions(_width, _height):
-	$Sprite2D.scale.x = 1.0 * _width  / $Sprite2D.texture.get_width() 
-	$Sprite2D.scale.y = 1.0 * _height / $Sprite2D.texture.get_height()
+func set_dimensions():
+	$Sprite2D.scale.x = 1.0 * width  / $Sprite2D.texture.get_width() 
+	$Sprite2D.scale.y = 1.0 * height / $Sprite2D.texture.get_height()
 
 # change the appearance
 func set_color(_color):
