a2e70046cefba475e2a0933724420d28d243484d
CrouzetC <43441902+CrouzetC@users.noreply.github.com>
2024-08-18 20:57:06 +0200

    correction level 3

--- Scenes/Levels/Level_3.gd
+++ Scenes/Levels/Level_3.gd
@@ -30,7 +30,7 @@ func validate_grid():
 		for i_x in range(nb_columns):
 			if colors_grid[i_y][i_x] == "yellow":
 				count_yellow += 1
-	return false #count_yellow == 0
+	return count_yellow == 0
 
 func get_init_grid():
 	return init_grid
